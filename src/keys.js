7635130052f1e6ec8158a8352c21b36a1a630f0b
Pablo Funes <pablo_funes09@hotmail.com>
2020-05-07 00:07:52 -0500

    Version 1.0

---
+++ src/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        host: 'remotemysql.com',
+        user: 'jmjBj6m4tO',
+        password: 'mbsUpzxZwY',
+        port: 3306,
+        database: 'jmjBj6m4tO'
+    }
+
+};
